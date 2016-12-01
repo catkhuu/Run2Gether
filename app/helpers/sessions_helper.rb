5e00cb4d5b231ceb6157948df617f95fdf786777
Catherine Khuu <sunshine.dumbo@gmail.com>
2016-11-30 19:37:59 -0500

    Fix side nav bar for when User is inbetween onboarding process. See sessions helper methods.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -36,4 +36,20 @@ module SessionsHelper
   def store_location
     session[:forarding_url] = request.original_url if request.get?
   end
+
+  def load_profile_picture
+    if current_user.profile
+      current_user.profile.avatar.url(:thumb)
+    else
+      profile_picture = 'http://www.freeiconspng.com/uploads/shoe-icon-27.png'
+    end
+  end
+
+  def create_or_view_profile
+    if current_user.profile
+      "/users/#{current_user.id}/profiles/#{current_user.profile.id}"
+    else
+      "/users/#{current_user.id}/profiles/new"
+    end
+  end
 end
