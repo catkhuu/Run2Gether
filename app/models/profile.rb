eb03f8f6a8c82c50956a36fce96f00c7d56208e4
catkhuu <sunshine.dumbo@gmail.com>
2017-01-10 17:32:10 -0500

    Validate format of zip code.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,6 +4,6 @@ class Profile < ApplicationRecord
   validates :experience, :user_id, presence: true
   validates :goals, length: { minimum: 15, maximum: 200 }, presence: true
   validates :need_to_knows, length: { maximum: 200 }
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/running_shoe.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "http://www.freeiconspng.com/uploads/shoe-icon-27.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
