1b57fce87a372834b384dc06b32cf303f2cb718a
catkhuu <sunshine.dumbo@gmail.com>
2017-01-10 17:33:17 -0500

    Validate format of zip code to allow zips that start with 0. Change geocode to after validation in case zip validation fails.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,13 @@ class User < ApplicationRecord
   has_many :runs, foreign_key: :runner_id
   has_many :runs_as_companion, foreign_key: :companion_id, class_name: 'Run'
 
-  validates :name, :zipcode, presence: true
-  validates :zipcode, length: { minimum: 5 , maximum: 5 }
+  validates :name, :zipcode, :latitude, :longitude, presence: true
+  validates_format_of :zipcode, :with => /\A\d{5}-\d{4}|\A\d{5}\z/, :message => "should be in the form 12345 or 12345-1234"
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }, uniqueness: true, presence: true
   validates :password, length: { minimum: 8, maximum: 20 }
   validates :password_confirmation, presence: true
   geocoded_by :zipcode
-  before_validation :geocode
-  # after_validation :geocode
+  after_validation :geocode
 
   # def narrow_by_experience(run_seeker)
   #   self.profile.experience == run_seeker.profile.experience
