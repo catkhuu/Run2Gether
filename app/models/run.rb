0f0b968237ae55a982e3db01eb7c518e3f004dea
catkhuu <sunshine.dumbo@gmail.com>
2017-01-11 17:26:51 -0500

    Add methods to narrow runs search and to queue user's run history and upcoming runs.

--- app/models/run.rb
+++ app/models/run.rb
@@ -19,4 +19,32 @@ class Run < ApplicationRecord
   def run_daypart
   end
 
+  def self.run_history(user)
+    Run.where(runner: user).or(Run.where(companion: user)).order('run_date ASC')
+  end
+
+  def self.past_runs(runs)
+    runs.select { |run| run.converted_date < DateTime.now }
+  end
+
+  def self.upcoming_runs(runs)
+    runs.select { |run| run.converted_date > DateTime.now }
+  end
+
+  def self.nearby_runs(run)
+    Run.near([run.latitude, run.longitude],1,:order => :distance)
+  end
+
+  def self.runs_on_date(searched_run, runs)
+    runs.where(run_date: searched_run.run_date)
+  end
+
+  def self.runs_by_experience(available_runs, user)
+    available_runs.joins(runner: :profile).where(profile: { experience: user.profile.experience })
+  end
+
+  def self.search_results(my_available_runs, user)
+    my_available_runs.where(companion_id: nil).where.not(runner: user)
+  end
+
 end
