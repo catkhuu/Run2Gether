e5bd77d3a767a8a9d99fa8d1789625772c44cb4c
catkhuu <sunshine.dumbo@gmail.com>
2017-01-11 17:00:43 -0500

    Clean up code for run creation and run search. Methods more in line with AR and are in helpers/models.

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,5 +1,4 @@
 class RunsController < ApplicationController
-  include RunsHelper
   before_action :sanitize_params, only: [:create, :update]
 
   def new
@@ -12,19 +11,18 @@ class RunsController < ApplicationController
   def create
     @run = Run.new(run_params)
     if @run.save
-      users_runs = Run.all.select { |run| run.runner_id == current_user.id ||      run.companion_id == current_user.id }
-      @upcoming_runs = users_runs.select { |run| run.converted_date > DateTime.now }
+      users_runs = Run.run_history(current_user)
+      @upcoming_runs = Run.upcoming_runs(users_runs)
       if request.xhr?
-        render partial: 'users/upcoming_runs', layout: false, locals: {upcoming_runs: @upcoming_runs}
+        render partial: 'users/upcoming_runs', layout: false, locals: { upcoming_runs: @upcoming_runs }
       else
         redirect_to users_show_path(current_user)
       end
     else
+      @errors = @run.errors.full_messages
       if request.xhr?
-        @errors = @run.errors.full_messages.to_json
-        render :json => @errors
+        render json: { errors: @errors }, status: 422
       else
-        @errors = @run.errors.full_messages
         render 'new'
       end
     end
@@ -37,12 +35,13 @@ class RunsController < ApplicationController
   end
 
   def search
-    temporary_run = Run.create(run_params)
-    by_proximity = Run.near([temporary_run.latitude, temporary_run.longitude],1,:order => :distance)
-    by_date = by_proximity.where(run_date: temporary_run.run_date)
-    search_results = by_date.select { |run| run.runner.profile.experience == current_user.profile.experience }
+    binding.pry
+    searched_run = Run.create(run_params)
+    nearby_runs = Run.nearby_runs(searched_run)
+    runs_on_date = Run.runs_on_date(searched_run, nearby_runs)
+    my_level_runs = Run.runs_by_experience(runs_on_date, current_user)
 
-    @final = search_results.select {|run| run.companion_id == nil && run.runner_id != current_user.id }.sample
+    @final = Run.search_results(my_level_runs, current_user).sample
     if @final
       if request.xhr?
         render 'users/_match', layout: false, locals: { final: @final }
@@ -59,14 +58,6 @@ class RunsController < ApplicationController
     end
   end
 
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-  end
   def add_companion
     if run = Run.where(id: params[:run_id]).update(companion_id: current_user.id)
       users_runs = Run.all.select { |run| run.runner_id == current_user.id || run.companion_id == current_user.id }
@@ -100,7 +91,6 @@ class RunsController < ApplicationController
     end
   end
 
-
   private
 
   def stats_params
@@ -119,8 +109,8 @@ class RunsController < ApplicationController
     if params[:run][:run_daypart] == 'PM'
       params[:run][:run_hours] = params[:run][:run_hours].to_i + 12
     end
-    hr_sec = convert_hours_to_seconds(params[:run][:run_hours])
-    min_sec = convert_minutes_to_seconds(params[:run][:run_minutes])
+    hr_sec = helpers.convert_hours_to_seconds(params[:run][:run_hours])
+    min_sec = helpers.convert_minutes_to_seconds(params[:run][:run_minutes])
     params[:run][:time] = hr_sec + min_sec
     params[:run][:runner_id] = current_user.id
   end
